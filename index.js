683fa81eee96ec23ed1b55c1050c651c21bf430c
Blake <blakewalters53@gmail.com>
2024-06-14 00:36:36 -0400

    Create index.js

---
+++ index.js
@@ -0,0 +1,7 @@
+// Assume using React.js for the frontend
+
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './App';
+
+ReactDOM.render(<App />, document.getElementById('app'));
